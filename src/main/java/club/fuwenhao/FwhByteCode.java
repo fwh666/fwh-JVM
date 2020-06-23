cb3b0adfd1a4085b99664e435ea212d1b48b7c8a
fuwenhao-mac <fuwenhao594@163.com>
2020-06-23 13:43:53 +0800

    新增：jvm字节码反编译demo

---
+++ src/main/java/club/fuwenhao/FwhByteCode.java
@@ -0,0 +1,21 @@
+package club.fuwenhao;
+
+/**
+ * 字节码研究demo
+ *
+ * @author fwh
+ * @email fuwenhao594@163.com
+ * @date 2020/6/22 2:01 下午
+ */
+public class FwhByteCode {
+    private String userName;
+
+    public String getUserName() {
+
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
